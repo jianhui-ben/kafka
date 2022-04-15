a2048250dab6e9daeaf983ca51ee39a11cbc221c
Jianhui Ben <jianhui.ben@appdynamics.com>
2022-04-15 14:40:42 -0700

    add seek and assign demo

---
+++ src/main/java/com/github/jianhuiben/kafka/quickStart/ConsumerDemoAssignSeek.java
@@ -0,0 +1,68 @@
+package com.github.jianhuiben.kafka.quickStart;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Array;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoAssignSeek {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignSeek.class);
+        Properties properties = new Properties();
+        String bootstrapServers = "localhost:9092";
+        String topic  = "firstTopic";
+
+        // consumer properties: https://docs.confluent.io/platform/current/installation/configuration/consumer-configs.html
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        //earliest: from the beginning; latest: message onwards
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        //assign and seek are mostly used to relay data or fetch a specific message
+
+        // assign to a specific partition of a topic
+        TopicPartition partitionToReadFrom = new TopicPartition(topic, 0);
+        long offsetToReadFrom = 15L;
+        consumer.assign(Arrays.asList(partitionToReadFrom));
+
+        //seek to a specific offset
+        consumer.seek(partitionToReadFrom, offsetToReadFrom);
+
+        int numberOfMessagesToRead = 5;
+        boolean keepOnReading = true;
+        int numberOfMessagesReadSoFar = 0;
+
+
+        //poll for new data
+        while(keepOnReading) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record: records) {
+                numberOfMessagesReadSoFar += 1;
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                if (numberOfMessagesReadSoFar >= numberOfMessagesToRead) {
+                    keepOnReading = false; //to exit the while loop
+                    break;  //to exit the for loop
+                }
+            }
+        }
+        logger.info("Exiting the application.");
+    }
+
+}
