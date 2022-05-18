6cf2d53aad477f2880cefcff35bdc2f6aef96628
Jianhui Ben <jianhui.ben@gmail.com>
2022-05-17 22:43:12 -0700

    add elastic search cleint

--- KafkaTwitterProducer/src/main/java/kafka/twitterProject/twitterProducer.java
+++ KafkaTwitterProducer/src/main/java/kafka/twitterProject/twitterProducer.java
@@ -53,7 +53,6 @@ public class twitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-
         // create a safe producer
         properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
